a4f09043a5e10870824d0e1bb9552d3d7e7e94db
Manoon666 <manoon8412@gmail.com>
2025-02-11 15:44:30 +0700

    labh19fished

--- lab19.h
+++ lab19.h
@@ -68,7 +68,41 @@ void Unit::newTurn(){
 /////////////////////////////////////////////////////////////////////////////////////
 //Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
 /////////////////////////////////////////////////////////////////////////////////////
+bool Unit::isDead(){
+    if(hp <= 0) return true;
+    else return false;
+}
+void Unit::guard(){
+    guard_on = true;
+}
+int Unit::beAttacked(int oppatk){
+    int damage = oppatk - def;
+    if(guard_on == false){
+        hp = hp - damage;
+        return damage;
+    }
+    else{
+        hp = hp - damage/3;
+        return damage/3;
+    }
 
+
+
+
+}
+int Unit::attack(Unit &name){
+	return name.beAttacked(atk);
+}
+int Unit :: heal(){
+    int hptemp = hp;
+    int add = rand()%21 +10;
+    hp = hp + add;
+    if(hp>=hpmax){
+        hp = hpmax;
+    }
+    
+    return hp-hptemp;
+}
 
 
 void drawScene(char p_action,int p,char m_action,int m){
